efed7cb57316f42fa4bd13a761030e572989b66e
Thomas <uqdll@student.kit.edu>
2018-01-01 13:08:55 +0100

    Erweiterung der Benutzereingabe
    Ich habe die Benutzereingabe in C# erweitert so das man den benutzer abfragen kann, was er für Signale erkennt.
    Dabei ist mir aufgefallen, dass bei der Berechnung der Zonen es vorkommen kann, dass es negative Zahlen auftauchen, das muss ich mir in Ruhe noch mal angucken. Siehe TODO

---
+++ C#/EA3/EA3/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace EA3
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SignalTyp untypedSignal;
+        MainProgram setup;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            initCode();
+            setup = new MainProgram();
+
+            //textBlock.Text = setup.playSignalStart();
+
+            
+        }
+
+        private void initCode()
+        {
+            untypedSignal = SignalTyp.NODATA;
+        }
+
+        private async void NextSignalButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            /*MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream =
+                await synth.SynthesizeTextToStreamAsync("Hello, World!");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();*/
+
+            // Falls nicht auf Next gedrückt worden ist, dann soll eine Fehler meldung erscheinen.
+            if (untypedSignal == SignalTyp.NODATA) {
+                var dialog = new MessageDialog("Sie haben Keine Eingabe getätigt \n" +
+                    "Bitte waehlen Sie Ihre Auswahl und bestaetigen Sie Ihre Eingabe.");
+                await dialog.ShowAsync();
+            } else {
+                setup.nextSignal(untypedSignal);
+                switch (untypedSignal)
+                {
+                    case SignalTyp.KURZ:
+                        radioButtonKurz.IsChecked = false; 
+                    break;
+                    case SignalTyp.MITTEL:
+                        radioButtonMittel.IsChecked = false;
+                    break;
+                    case SignalTyp.LANG:
+                        radioButtonLang.IsChecked = false;
+                    break;
+                    default:
+                        Debug.WriteLine("ERROR in der  NextSignalButton_Click_1 Methode");
+                    break;
+                }
+
+                NextSignalButton.IsEnabled = false;
+                playSignalButton.IsEnabled = true;
+                untypedSignal = SignalTyp.NODATA;
+            }
+        }
+
+        private void playSignalButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (setup.isStartElementAvailable())
+            {
+                String s = setup.playStartSignal();
+                textBlock.Text = s;
+                NextSignalButton.IsEnabled = true;
+                playSignalButton.IsEnabled = false;
+            } else {
+                NextSignalButton.IsEnabled = true;
+                // Eingabe des Benutzers ist fertig für die validierung der gleichverteilten Population. 
+                // Es kann jetzt die Berechnung der Grenzen für Kurz, Mittel und Lang erfolgen.
+                setup.calculateStartZones();
+            }
+        }
+
+        private void radioButtonKurz_Checked(object sender, RoutedEventArgs e)
+        {
+            untypedSignal = SignalTyp.KURZ;
+        }
+
+        private void radioButtonMittel_Checked(object sender, RoutedEventArgs e)
+        {
+            untypedSignal = SignalTyp.MITTEL;
+        }
+
+        private void radioButtonLang_Checked(object sender, RoutedEventArgs e)
+        {
+            untypedSignal = SignalTyp.LANG;
+        }
+    }
+}
---
+++ C#/EA3/EA3/Signal.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EA3
+{
+    /**
+     * Die Klasse Signal repraesentiert ein Signal das nur aus drei Signalen zur verfuegung Steht: 
+     * kurz, mittel und lang
+     * Jedes Signal hat eine mindest und maximal laenge
+     * @author thomas
+     */
+    public enum SignalTyp
+    {
+        KURZ,
+        MITTEL,
+        LANG,
+        NODATA
+    }
+
+    public class Signal
+    {
+
+        private SignalTyp type;
+        private int time;           // Zeit in ms
+        private string signalCode;  // 
+        private char[] cSignalCode; // 
+        private int iNull;          // Anzahl der Nullen
+        private int iEins;          // Anzahl der Einsen
+        private int begin;          // MinimalZeit fÃ¼r den Typ von Signal
+        private int end;            // MaximalZeit fÃ¼r den Typ von Signal
+
+        /**
+         * erzeugt ein Signal, dass mit der genannten Zeit
+         * @param sTime Zeit in ms
+         */
+        public Signal(int sTime)
+        {
+            type = SignalTyp.NODATA;
+            time = sTime;
+            init();
+        }
+
+        /**
+         * Konstruktor des Signals
+         * @param sType Signaltyp 
+         * @param sTime Zeit in ms, die sich in dem Intervall des Signaltyps befinden soll
+         */
+        public Signal(SignalTyp sType, int sTime)
+        {
+            type = sType;
+            time = sTime;
+            //cSignalCode = new char [(end/5)];
+            init();
+        }
+
+        /**
+         * Initialisierungsmethode
+         */
+        private void init()
+        {
+            rightTime();
+            calculate();
+            createString();     // erzeuge den Code
+        }
+
+        /**
+         * es wird fuer jeden Signaltyp die Intervallgrenzen bestimmt
+         */
+        private void rightTime()
+        {
+            switch (type)
+            {
+                case SignalTyp.KURZ:
+                    begin = MainProgram.MINKURZTIME;
+                    end = MainProgram.MAXKURZTIME;
+                    break;
+                case SignalTyp.MITTEL:
+                    begin = MainProgram.MINMITTELTIME;
+                    end = MainProgram.MAXMITTELTIME;
+                    break;
+                case SignalTyp.LANG:
+                    begin = MainProgram.MINLANGTIME;
+                    end = MainProgram.MAXLANGTIME;
+                    break;
+                case SignalTyp.NODATA:
+                    begin = MainProgram.MINTIME;
+                    end = MainProgram.MAXTIME;
+                    break;
+            }
+        }
+
+        /**
+         * Berechnet die Anzahl der Einsen und Nullen
+         */
+        private void calculate()
+        {
+            iEins = time / 5;
+            iNull = (end / 5) - (time / 5);
+        }
+
+        private void  createString()
+        {
+            char[] test = new char[(end / 5)];
+            for (int i = 0; i < iEins; i++)
+            {
+                test[i] = '1';
+            }
+
+            for (int i = iEins; i < (end / 5); i++)
+            {
+                test[i] = '0';
+            }
+
+            cSignalCode = test;
+            signalCode = new string(cSignalCode);
+            // Ausgabe
+            Debug.WriteLine("Ausgabe von caltulateString() Zeit : " + time + " berechnete einsen " + iEins + " daraus folgt "
+                    + getEins() + " Code = ");
+            Debug.WriteLine(signalCode);
+            //return signalCode;
+        }
+
+        /**
+         * erstellt den neuen String und gibt ihn Aus.
+         */
+        public String printString()
+        {
+            createString();
+            return signalCode;
+        }
+
+        /**
+         * Setze die Anzahl (n) an Nullen
+         * @param n Anzahl Einsen die gesetzt werden soll
+         */
+        public void setEins(int n)
+        {
+            iEins = n;
+        }
+
+        /**
+         * setzt die Anzahl (n) an Nullen
+         * @param n Anzahl Nullen die gesetzt werden soll
+         */
+        public void setNull(int n)
+        {
+            iNull = n;
+        }
+
+        /**
+         * setzt den Typen des Signals 
+         * @param sType ist der Typ der gesetzt werden soll
+         */
+        public void setType(SignalTyp sType)
+        {
+            type = sType;
+        }
+
+        /**
+         * gibt die Anzahl der Einsen an
+         * @return
+         */
+        public int getEins()
+        {
+            return iEins;
+        }
+
+        /**
+         * gibt die Anzahl der Nullen an 
+         * @return
+         */
+        public int getNull()
+        {
+            return iNull;
+        }
+
+        /**
+         * gibt den Typ des Signals zurueck
+         * @return
+         */
+        public SignalTyp getType()
+        {
+            return type;
+        }
+
+        /**
+         * gibt die Zeit des Signals von dem Signaltyp zurueck  
+         * @return
+         */
+        public int getTime()
+        {
+            return time;
+        }
+
+        /**
+         * Gibt die Mindestzeit des Intervalls des Signalstypen an
+         * @return
+         */
+        public int minTime()
+        {
+            return begin;
+        }
+
+        /**
+         * gibt den Endzeitpunkt des Intervalls dieses Signaltyps an
+         * @return
+         */
+        public int maxTime()
+        {
+            return end;
+        }
+
+        // Anzahl Einsen
+        /*public int countOnce() {
+			//int temp = signalCode.length() - signalCode.replace("1", "").length();
+			return iEins;
+		}*/
+    }
+}
